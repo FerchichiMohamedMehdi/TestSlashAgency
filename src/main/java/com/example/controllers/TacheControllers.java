63aa94bf7b63a9d6f6a361e8f0d712926a9c03ac
FerchichiMohamedMehdi <mohamedmehdi.ferchichi@etudiant-isi.utm.tn>
2023-11-14 22:18:52 +0100

    controllers + config DB

--- src/main/java/com/example/controllers/TacheControllers.java
+++ src/main/java/com/example/controllers/TacheControllers.java
@@ -1,4 +1,32 @@
 package com.example.controllers;
 
+import com.example.entities.Tache;
+import com.example.services.TacheService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/Tache")
 public class TacheControllers {
+
+
+
+        @Autowired
+        TacheService tacheService;
+        @PostMapping("/add")
+        public void addOrUpdateTache (@RequestBody Tache tache){
+            tacheService.addOrUpdateTache(tache);
+        }
+        @DeleteMapping("/delete/{idTache}")
+        public void deleteTache(@PathVariable("idTache")Long idTache){
+            tacheService.deleteTache(idTache);
+        }
+        @CrossOrigin(origins = "*")
+        @GetMapping("/listAll")
+        public List<Tache> listAllTache(){
+            return tacheService.getAllTaches();
+        }
 }
