5818bd1013feb63dd3b555371f85b5694cc0cad1
FerchichiMohamedMehdi <mohamedmehdi.ferchichi@etudiant-isi.utm.tn>
2023-11-25 23:32:02 +0100

    repo

--- src/main/java/com/example/testslashagency/repositories/TacheRepository.java
+++ src/main/java/com/example/testslashagency/repositories/TacheRepository.java
@@ -4,5 +4,6 @@ import com.example.testslashagency.entities.Tache;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TacheRepository extends JpaRepository<Tache, Long> {
+    public Tache getTacheById(Long id);
 
 }
